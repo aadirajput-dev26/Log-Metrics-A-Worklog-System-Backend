7693a13bffa01fee0571d2d00f534be4f2908378
Aadityaraj Singh Mandloi <aadityarajsinghmandloi104@gmail.com>
2026-04-03 00:15:26 +0530

    connectDB() function changed for debugging purposes

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
 export const connectDB = async () => {
     try{
         const connect = await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected successfully");
+        console.log(`MongoDB connected to: ${process.env.MONGODB_URI}`)
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1);
