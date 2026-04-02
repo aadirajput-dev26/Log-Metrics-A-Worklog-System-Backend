8cfb621aad4003c09c9bda74419ebe6ab9ef9dec
Aadityaraj Singh Mandloi <aadityarajsinghmandloi104@gmail.com>
2026-04-03 00:22:46 +0530

    Removed validation from connectDB

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,13 +3,8 @@ import "dotenv/config";
 
 export const connectDB = async () => {
     try{
-        const connect = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000
-        });
+        const connect = await mongoose.connect(process.env.MONGODB_URI);
         console.log("MongoDB connected successfully");
-        console.log(`MongoDB connected to: ${process.env.MONGODB_URI}`)
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1);
