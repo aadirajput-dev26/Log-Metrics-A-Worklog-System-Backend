731849eb8c271f3cacc75596dfda88dc9356f194
Aadityaraj Singh Mandloi <aadityarajsinghmandloi104@gmail.com>
2026-04-03 03:35:40 +0530

    CORS error handled

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import authRoutes from "./src/routes/auth.route.js";
 import workDayRoutes from "./src/routes/workDay.route.js";
 import activityRecordRoutes from "./src/routes/activityRecord.route.js";
 import userProfileRoutes from "./src/routes/user.route.js"
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,12 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "https://log-metrics.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/work-day" , workDayRoutes);
