c1a39f5770d8431fa5c4d33d2524e06a93b28a18
Aadityaraj Singh Mandloi <aadityarajsinghmandloi104@gmail.com>
2026-05-27 22:25:08 +0530

    Remove temporary helper scripts

--- updateActivity.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-let code = fs.readFileSync('src/models/activityRecord.js', 'utf8');
-code = code.replace(/enum\s*:\s*\[\s*"TASK"\s*,\s*"BREAK"\s*\]/g, 'enum : ["TASK", "BREAK", "MEETING", "LEARNING", "PLANNING"]');
-fs.writeFileSync('src/models/activityRecord.js', code);
-console.log('Done');
